4ece2fee61f386ad7c72ed3a4c92ca25a77234d1
Romuald Guyonnet-Duluc <meetnshred@gmail.com>
2020-05-10 21:34:23 -0400

    fix op name , no space

--- src/main/java/com/romu31/camelr31opentracing/OpenTracingRestController.java
+++ src/main/java/com/romu31/camelr31opentracing/OpenTracingRestController.java
@@ -1,21 +1,30 @@
 package com.romu31.camelr31opentracing;
 
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class OpenTracingRestController {
 
 	@Autowired
 	ProducerTemplate producerTemplate;
 
 	@GetMapping("/one")
-	public String test(){
+	public String test(@RequestHeader Map<String, String> headers){
+		headers.forEach((key, value) -> {
+			log.info(String.format("Header '%s' = %s", key, value));
+		});
 		producerTemplate.sendBody("direct:writeQueue", "<hello>world!</hello>");
 		return "ok";
 	}
